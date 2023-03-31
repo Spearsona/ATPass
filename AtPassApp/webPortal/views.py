c1826b21e0e2db0493d840f262a3bf62e7a6ea47
Spearsona <c17753589@mytudublin.ie>
2023-03-31 13:31:54 +0100

    Added Homepage

--- AtPassApp/webPortal/views.py
+++ AtPassApp/webPortal/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+app_name = 'webPortal'
+
+def index(request):
+    context={}
+    return render(request, 'webPortal/index.html', context) 
